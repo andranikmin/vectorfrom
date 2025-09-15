591afdbda674dc3f96755be0cfcdaebb081f3e69
Andranik Minasyan <minasyan.andranik@softconstruct.com>
2025-09-15 13:27:35 +0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { HowToOrderComponent } from './components/how-to-order/how-to-order.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    AboutUsComponent,
+    HowToOrderComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
